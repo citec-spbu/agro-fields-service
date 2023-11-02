f98ef7507b07b94195638d3b0351e917436ffc34
asavershin <avershinrussian@gmail.com>
2023-11-02 18:53:26 +0300

    Remove useless coments

--- src/main/java/agroscience/fields/dao/repositories/CropsRepository.java
+++ src/main/java/agroscience/fields/dao/repositories/CropsRepository.java
@@ -5,14 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface CropsRepository extends JpaRepository<Crop, Long> {
-//    @Query("SELECT c FROM Crop c " +
-//            "WHERE c.id = (SELECT cr.crop.id FROM CropRotation cr " +
-//            "WHERE cr.field.id = :fieldId " +
-//            "ORDER BY cr.startDate DESC " +
-//            "LIMIT 1)")
-//    Crop findLatestCrop(Long fieldId);
-
     Crop findCropById(Long cropId);
-
-
 }
