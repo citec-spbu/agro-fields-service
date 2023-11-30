772920086512325b1a3bbdbb9d030b9aa5cf0866
asavershin <avershinrussian@gmail.com>
2023-11-30 16:07:01 +0300

    Add new exception and default valid message

---
+++ src/main/java/agroscience/fields/exceptions/ExceptionBody.java
@@ -0,0 +1,17 @@
+package agroscience.fields.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+public class ExceptionBody {
+    private String message;
+    private Map<String, String> errors;
+
+    public ExceptionBody(final String message) {
+        this.message = message;
+    }
+}
