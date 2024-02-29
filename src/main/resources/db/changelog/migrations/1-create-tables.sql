3f49053216595ab9d47ad8396a1520ab8f00bdd3
asavershin <avershinrussian@gmail.com>
2024-02-29 22:50:48 +0300

    Change migrations

--- src/main/resources/db/changelog/migrations/1-create-tables.sql
+++ src/main/resources/db/changelog/migrations/1-create-tables.sql
@@ -2,14 +2,14 @@
 
 -- changeset asavershin:createTables
 
-CREATE TABLE crop (
+CREATE TABLE if not exists crop (
                        crop_id bigserial PRIMARY KEY,
                        crop_name VARCHAR(50) NOT NULL UNIQUE
 );
 
-CREATE TABLE field (
+CREATE TABLE if not exists field (
                         field_id bigserial PRIMARY KEY,
-                        field_name VARCHAR(100) NOT NULL UNIQUE,
+                        field_name VARCHAR(100) NOT NULL,
                         field_square_area VARCHAR(40) NOT NULL,
                         field_geom GEOMETRY NOT NULL,
                         field_description VARCHAR(256),
@@ -19,7 +19,7 @@ CREATE TABLE field (
                         field_organization_id INTEGER NOT NULL
 );
 
-CREATE TABLE crop_rotation (
+CREATE TABLE if not exists crop_rotation (
                                crop_rotation_id BIGSERIAL PRIMARY KEY,
                                field_id bigint NOT NULL,
                                crop_id bigint NOT NULL,
@@ -30,11 +30,11 @@ CREATE TABLE crop_rotation (
                                 FOREIGN KEY (crop_id) REFERENCES crop(crop_id)
 );
 
-CREATE TABLE soil (
+CREATE TABLE if not exists soil (
                       soil_id bigserial PRIMARY KEY,
                       field_id bigint NOT NULL,
                       soil_ph VARCHAR(10),
-                      soil_sample_date DATE UNIQUE NOT NULL,
+                      soil_sample_date DATE NOT NULL,
                       soil_organic_matter VARCHAR(10),
                       soil_mobile_p VARCHAR(10),
                       soil_mobile_k VARCHAR(10),
