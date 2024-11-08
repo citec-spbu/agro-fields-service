3cf77cdf75cc7cd1568060e112284f077eb805a8
KABOPOK <mishavvvsweater@gmail.com>
2024-11-08 21:30:24 +0300

    add controller methods

--- src/main/java/agroscience/fields/v2/repositories/SoilCompositionsRepository.java
+++ src/main/java/agroscience/fields/v2/repositories/SoilCompositionsRepository.java
@@ -1,11 +1,16 @@
 package agroscience.fields.v2.repositories;
 
+import agroscience.fields.v2.entities.Contour;
+import agroscience.fields.v2.entities.FieldV2;
 import agroscience.fields.v2.entities.SoilComposition;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SoilCompositionsRepository extends JpaRepository<SoilComposition, UUID> {
+
+  List<SoilComposition> findAllByContourAndArchivedIsFalse(Contour contour);
 
 }
