893310b11e88888812280e7b9b3ddf7da257d603
KABOPOK <mishavvvsweater@gmail.com>
2024-11-10 00:05:21 +0300

    73 make all test for controller methods

--- src/test/java/agroscience/fields/SoilCompositionTest.java
+++ src/test/java/agroscience/fields/SoilCompositionTest.java
@@ -1,26 +1,37 @@
 package agroscience.fields;
 
-import agroscience.fields.dto.field.CoordinatesDTO;
-import agroscience.fields.dto.field.GeomDTO;
-import agroscience.fields.v2.entities.Contour;
 import agroscience.fields.v2.entities.FieldV2;
 import agroscience.fields.v2.entities.Season;
 import agroscience.fields.v2.entities.SoilComposition;
+import agroscience.fields.v2.mappers.SoilCompositionMapper;
 import agroscience.fields.v2.repositories.FieldsRepository;
 import agroscience.fields.v2.repositories.SeasonsRepository;
 import agroscience.fields.v2.repositories.SoilCompositionsRepository;
-import agroscience.fields.v2.services.SoilCompositionsService;
+import generated.agroscience.fields.api.model.IdDTO;
+import generated.agroscience.fields.api.model.SoilCompositionDTO;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.locationtech.jts.geom.Coordinate;
-import org.locationtech.jts.geom.Geometry;
-import org.locationtech.jts.geom.GeometryFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
-import java.time.LocalDate;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.util.UriComponentsBuilder;
 import java.util.List;
 import java.util.UUID;
-import static org.junit.jupiter.api.Assertions.*;
+import static agroscience.fields.SampleObjectGenerator.createSampleFieldAndContourInside;
+import static agroscience.fields.SampleObjectGenerator.createSampleSeason;
+import static agroscience.fields.SampleObjectGenerator.createSampleSoilComposition;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class SoilCompositionTest extends AbstractTest {
 
   @Autowired
@@ -28,114 +39,211 @@ public class SoilCompositionTest extends AbstractTest {
   @Autowired
   private FieldsRepository fieldRepository;
   @Autowired
-  private SoilCompositionsService soilCompositionsService;
-  @Autowired
   private SoilCompositionsRepository soilCompositionsRepository;
-  public static Geometry geom() {
-    CoordinatesDTO coordinates1 = new CoordinatesDTO(1.0, 2.0);
-    CoordinatesDTO coordinates2 = new CoordinatesDTO(3.0, 4.0);
-    CoordinatesDTO coordinates3 = new CoordinatesDTO(4.0, 3.0);
-    CoordinatesDTO coordinates4 = new CoordinatesDTO(1.0, 2.0);
+  @Autowired
+  private SoilCompositionMapper soilCompositionMapper;
+  private static final TestRestTemplate testRestTemplate = new TestRestTemplate();
+  private static final String TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3Mjk0OTQzNTQsImV4cCI6MTcyOTQ5Nzk1NCwic3ViIjoiYzUzNmQyODEtNTM5MC00YzViLWIxYTMtNjIwNjEzOWZjZTFlIiwicm9sZSI6Im9yZ2FuaXphdGlvbiIsImVtYWlsIjoidGVzdEB0ZXN0LnJ1Iiwib3JnIjoiYzUzNmQyODEtNTM5MC00YzViLWIxYTMtNjIwNjEzOWZjZTFlIn0.0pXuoEo35KiJVYStWFz7cM2UMSbwe19vyZNtIoLDpHs";
 
-    List<CoordinatesDTO> coordinatesList = List.of(coordinates1, coordinates2, coordinates3, coordinates4);
-
-    var geomDto = new GeomDTO("Polygon", coordinatesList);
-
-    var coordinates = geomDto.getCoordinates();
-    GeometryFactory geometryFactory = new GeometryFactory();
-    // Преобразовать координаты в массив точек
-    Coordinate[] polygonCoordinates = new Coordinate[coordinates.size()];
-    for (int i = 0; i < coordinates.size(); i++) {
-      polygonCoordinates[i] =
-              new Coordinate(coordinates.get(i).getLongitude(), coordinates.get(i).getLatitude());
-    }
-    return geometryFactory.createPolygon(polygonCoordinates);
+  public static <T> ResponseEntity<T> sendPostRequest(Object entityDTO, String url, Class<T> responseType){
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    headers.setBearerAuth(TOKEN);
+    HttpEntity<Object> request = new HttpEntity<>(entityDTO, headers);
+    return testRestTemplate.exchange(
+            url,
+            HttpMethod.POST,
+            request,
+            responseType
+    );
   }
 
-  public static FieldV2 createSampleField(){
-    FieldV2 field = new FieldV2();
-    field.setFieldId(UUID.randomUUID());
-    field.setName("Test Field");
-    field.setDescription("Description of the test field");
-    Contour contour = new Contour();
-    contour.setContourId(UUID.randomUUID());
-    contour.setName("Test Contour");
-    contour.setSquareArea("1000");
-    contour.setColor("FF5733");
-    contour.setGeom(geom());
-    contour.setField(field);
-    field.setContours(List.of(contour));
-    return field;
-  }
-  private static Season createSampleSeason() {
-    Season season = new Season();
-    season.setSeasonId(UUID.randomUUID());
-    season.setName("potato");
-    season.setEndDate(LocalDate.now());
-    season.setArchived(false);
-    season.setStartDate(LocalDate.now());
-    season.setOrganizationId(UUID.randomUUID());
-    return  season;
+  public static <T> ResponseEntity<List<T>> sendGetRequest(String url, ParameterizedTypeReference<List<T>> responseType) {
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    headers.setBearerAuth(TOKEN);
+    HttpEntity<Object> request = new HttpEntity<>(headers);
+
+    return testRestTemplate.exchange(
+            url,
+            HttpMethod.GET,
+            request,
+            responseType
+    );
   }
 
-  public static SoilComposition createSampleSoilComposition(Contour contour) {
-    SoilComposition soilComposition = new SoilComposition();
-    soilComposition.setSoilCompositionId(UUID.randomUUID());
-    soilComposition.setPh("6.5");
-    soilComposition.setSampleDate(LocalDate.now());
-    soilComposition.setOrganicMatter("5.2%");
-    soilComposition.setMobileP("12.5 mg/kg");
-    soilComposition.setMobileK("80 mg/kg");
-    soilComposition.setMobileS("15.3 mg/kg");
-    soilComposition.setNitrateN("5.1 mg/kg");
-    soilComposition.setAmmoniumN("2.3 mg/kg");
-    soilComposition.setHydrolyticAcidity("3.5 cmol/kg");
-    soilComposition.setCaExchange("15.2 cmol/kg");
-    soilComposition.setMgExchange("7.8 cmol/kg");
-    soilComposition.setB("0.8 mg/kg");
-    soilComposition.setCo("0.05 mg/kg");
-    soilComposition.setMn("6.2 mg/kg");
-    soilComposition.setZn("1.1 mg/kg");
-    soilComposition.setContour(contour);  // Assigning a Contour
+  public static ResponseEntity<Void> sendDeleteRequest(UUID soilCompositionId, String url, String queryParamName) {
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    headers.setBearerAuth(TOKEN);
+    HttpEntity<Object> request = new HttpEntity<>(headers);
+    String urlWithParams = UriComponentsBuilder.fromHttpUrl(url)
+            .queryParam(queryParamName, soilCompositionId)
+            .toUriString();
+    return testRestTemplate.exchange(
+            urlWithParams,
+            HttpMethod.DELETE,
+            request,
+            Void.class
+    );
+  }
 
-    return soilComposition;
+  public static ResponseEntity<Void> sendPutRequest(UUID soilCompositionId, Object entityDTO, String url, String queryParamName) {
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    headers.setBearerAuth(TOKEN);
+    HttpEntity<Object> request = new HttpEntity<>(entityDTO, headers);
+    String urlWithParams = UriComponentsBuilder.fromHttpUrl(url)
+            .queryParam(queryParamName, soilCompositionId)
+            .toUriString();
+    return testRestTemplate.exchange(
+            urlWithParams,
+            HttpMethod.PUT,
+            request,
+            Void.class
+    );
+  }
+
+  @BeforeEach
+  @AfterEach
+  public void clear() {
+    fieldRepository.deleteAll();
+    soilCompositionsRepository.deleteAll();
+    fieldRepository.deleteAll();
   }
 
   @Test
-  @Transactional
-  public void createSoilTest() {
+  public void createSoilCompositionTest() {
     // Given
     Season season = createSampleSeason();
     seasonsRepository.save(season);
-    FieldV2 field = createSampleField();
-    field.setSeason(season);
+    FieldV2 field = createSampleFieldAndContourInside(season);
     fieldRepository.save(field);
     UUID contourId = field.getContours().get(0).getContourId();
     SoilComposition soilComposition1 = createSampleSoilComposition(field.getContours().get(0));
     SoilComposition soilComposition2 = createSampleSoilComposition(field.getContours().get(0));
+    SoilCompositionDTO soilCompositionDTO1 = soilCompositionMapper.map(soilComposition1);
+    SoilCompositionDTO soilCompositionDTO2 = soilCompositionMapper.map(soilComposition2);
     // When
-    soilCompositionsService.save(contourId,soilComposition1);
-    soilCompositionsService.save(contourId,soilComposition2);
+    String url = "http://localhost:8080/api/v2/fields-service/contours/" + contourId + "/soil-composition";
+    ResponseEntity<IdDTO> response1 = sendPostRequest(soilCompositionDTO1,url, IdDTO.class);
+    ResponseEntity<IdDTO> response2 = sendPostRequest(soilCompositionDTO2,url, IdDTO.class);
     // Then
     List<SoilComposition> savedSoilCompositions = soilCompositionsRepository.findAll();
-    assertNotNull(savedSoilCompositions, "Soil compositions should not be null");
-    assertEquals(2, savedSoilCompositions.size(), "There should be exactly two soil compositions for the contour");
-    assertTrue(savedSoilCompositions.stream()
-                    .anyMatch(soil -> soil.getSoilCompositionId().equals(soilComposition1.getSoilCompositionId())),
-            "Soil composition 1 should be in the saved list");
-    assertTrue(savedSoilCompositions.stream()
-                    .anyMatch(soil -> soil.getSoilCompositionId().equals(soilComposition2.getSoilCompositionId())),
-            "Soil composition 2 should be in the saved list");
-    assertEquals(contourId, savedSoilCompositions.get(0).getContour().getContourId(),
-            "Soil composition 1 should be linked to the correct contour");
-    assertEquals(contourId, savedSoilCompositions.get(1).getContour().getContourId(),
-            "Soil composition 2 should be linked to the correct contour");
-    assertNotNull(savedSoilCompositions.get(0).getSoilCompositionId(), "Soil composition 1 ID should be generated");
-    assertNotNull(savedSoilCompositions.get(1).getSoilCompositionId(), "Soil composition 2 ID should be generated");
-    assertNotNull(savedSoilCompositions.get(0).getSampleDate(), "Sample date for soil composition 1 should not be null");
-    assertNotNull(savedSoilCompositions.get(1).getSampleDate(), "Sample date for soil composition 2 should not be null");
-    assertEquals("6.5", savedSoilCompositions.get(0).getPh(), "pH value of soil composition 1 should match the expected value");
-    assertEquals("5.2%", savedSoilCompositions.get(1).getOrganicMatter(), "organicMatter value of soil composition 2 should match the expected value");
+    assertEquals(200, response1.getStatusCode().value());
+    assertEquals(200, response2.getStatusCode().value());
+    assertNotNull(response1.getBody());
+    assertNotNull(response2.getBody());
+    assertEquals(IdDTO.class, response1.getBody().getClass());
+    assertEquals(IdDTO.class, response2.getBody().getClass());
+    assertEquals(2, savedSoilCompositions.size());
+    assertEquals(contourId, savedSoilCompositions.get(0).getContour().getContourId());
+    assertEquals(contourId, savedSoilCompositions.get(1).getContour().getContourId());
+    assertEquals(response1.getBody().getId(), savedSoilCompositions.get(0).getSoilCompositionId());
+    assertEquals(response2.getBody().getId(), savedSoilCompositions.get(1).getSoilCompositionId());
+    assertNotNull(savedSoilCompositions.get(1).getSampleDate());
+    assertEquals("6.5", savedSoilCompositions.get(0).getPh());
+    assertEquals("5.2%", savedSoilCompositions.get(1).getOrganicMatter());
+  }
+
+  @Test
+  public void getSoilCompositionTest(){
+    // Given
+    Season season = createSampleSeason();
+    seasonsRepository.save(season);
+    FieldV2 field = createSampleFieldAndContourInside(season);
+    fieldRepository.save(field);
+    UUID contourId = field.getContours().get(0).getContourId();
+    SoilComposition soilComposition1 = createSampleSoilComposition(field.getContours().get(0));
+    SoilComposition soilComposition2 = createSampleSoilComposition(field.getContours().get(0));
+    soilCompositionsRepository.save(soilComposition1);
+    soilCompositionsRepository.save(soilComposition2);
+    // When
+    String url = "http://localhost:8080/api/v2/fields-service/contours/" + contourId + "/soil-compositions";
+    ResponseEntity<List<SoilCompositionDTO>> response1 = sendGetRequest(url, new ParameterizedTypeReference<>() {});
+    ResponseEntity<List<SoilCompositionDTO>> response2 = sendGetRequest(url, new ParameterizedTypeReference<>() {});
+    //Then
+    assertEquals(200, response1.getStatusCode().value());
+    assertEquals(200, response2.getStatusCode().value());
+    List<SoilCompositionDTO> responseDto1 = response1.getBody();
+    List<SoilCompositionDTO> responseDto2 = response2.getBody();
+    assertNotNull(responseDto1);
+    assertNotNull(responseDto2);
+    assertEquals(soilComposition1.getPh(), responseDto1.get(0).getPh());
+    assertEquals(soilComposition1.getSampleDate(), responseDto1.get(0).getSampleDate());
+    assertEquals(soilComposition1.getOrganicMatter(), responseDto1.get(0).getOrganicMatter());
+    assertEquals(soilComposition1.getMobileP(), responseDto1.get(0).getMobileP());
+    assertEquals(soilComposition1.getMobileK(), responseDto1.get(0).getMobileK());
+    assertEquals(soilComposition1.getMobileS(), responseDto1.get(0).getMobileS());
+    assertEquals(soilComposition1.getNitrateN(), responseDto1.get(0).getNitrateN());
+    assertEquals(soilComposition1.getAmmoniumN(), responseDto1.get(0).getAmmoniumN());
+    assertEquals(soilComposition1.getHydrolyticAcidity(), responseDto1.get(0).getHydrolyticAcidity());
+    assertEquals(soilComposition1.getCaExchange(), responseDto1.get(0).getCaExchange());
+    assertEquals(soilComposition1.getMgExchange(), responseDto1.get(0).getMgExchange());
+    assertEquals(soilComposition1.getB(), responseDto1.get(0).getB());
+    assertEquals(soilComposition1.getCo(), responseDto1.get(0).getCo());
+    assertEquals(soilComposition1.getMn(), responseDto1.get(0).getMn());
+    assertEquals(soilComposition1.getZn(), responseDto1.get(0).getZn());
+    assertEquals(soilComposition2.getPh(), responseDto1.get(0).getPh());
+    assertEquals(soilComposition2.getSampleDate(), responseDto2.get(0).getSampleDate());
+    assertEquals(soilComposition2.getOrganicMatter(), responseDto2.get(0).getOrganicMatter());
+    assertEquals(soilComposition2.getMobileP(), responseDto2.get(0).getMobileP());
+    assertEquals(soilComposition2.getMobileK(), responseDto2.get(0).getMobileK());
+    assertEquals(soilComposition2.getMobileS(), responseDto2.get(0).getMobileS());
+    assertEquals(soilComposition2.getNitrateN(), responseDto2.get(0).getNitrateN());
+    assertEquals(soilComposition2.getAmmoniumN(), responseDto2.get(0).getAmmoniumN());
+    assertEquals(soilComposition2.getHydrolyticAcidity(), responseDto2.get(0).getHydrolyticAcidity());
+    assertEquals(soilComposition2.getCaExchange(), responseDto2.get(0).getCaExchange());
+    assertEquals(soilComposition2.getMgExchange(), responseDto2.get(0).getMgExchange());
+    assertEquals(soilComposition2.getB(), responseDto2.get(0).getB());
+    assertEquals(soilComposition2.getCo(), responseDto2.get(0).getCo());
+    assertEquals(soilComposition2.getMn(), responseDto2.get(0).getMn());
+    assertEquals(soilComposition2.getZn(), responseDto2.get(0).getZn());
+  }
+
+  @Test
+  public void deleteSoilCompositionTest(){
+    // Given
+    Season season = createSampleSeason();
+    seasonsRepository.save(season);
+    FieldV2 field = createSampleFieldAndContourInside(season);
+    fieldRepository.save(field);
+    SoilComposition soilComposition1 = createSampleSoilComposition(field.getContours().get(0));
+    SoilComposition soilComposition2 = createSampleSoilComposition(field.getContours().get(0));
+    soilCompositionsRepository.save(soilComposition1);
+    soilCompositionsRepository.save(soilComposition2);
+    //When
+    String url = "http://localhost:8080/api/v2/fields-service/soil-composition";
+    ResponseEntity<Void> response = sendDeleteRequest(soilComposition1.getSoilCompositionId(),url, "soilCompositionId");
+    //Then
+    assertEquals(200, response.getStatusCode().value());
+    SoilComposition soilComposition = soilCompositionsRepository.findById(soilComposition1.getSoilCompositionId()).orElseThrow();
+    assertTrue(soilComposition.isArchived());
+    List<SoilComposition> savedSoilCompositions = soilCompositionsRepository.findAllByContourAndArchivedIsFalse(field.getContours().get(0));
+    assertEquals(1,savedSoilCompositions.size());
+  }
+
+  @Test
+  public void putSoilCompositionTest(){
+    // Given
+    Season season = createSampleSeason();
+    seasonsRepository.save(season);
+    FieldV2 field = createSampleFieldAndContourInside(season);
+    fieldRepository.save(field);
+    SoilComposition soilComposition1 = createSampleSoilComposition(field.getContours().get(0));
+    SoilComposition soilComposition2 = createSampleSoilComposition(field.getContours().get(0));
+    soilCompositionsRepository.save(soilComposition1);
+    soilCompositionsRepository.save(soilComposition2);
+    soilComposition1.setCo("KoKOKOkO");
+    SoilCompositionDTO soilCompositionDTO = soilCompositionMapper.map(soilComposition1);
+    //When
+    String url = "http://localhost:8080/api/v2/fields-service/soil-composition";
+    ResponseEntity<Void> response = sendPutRequest(soilComposition1.getSoilCompositionId(), soilCompositionDTO, url, "SoilCompositionId");
+    //Then
+    assertEquals(200, response.getStatusCode().value());
+    SoilComposition updatedSoilComposition = soilCompositionsRepository.findById(soilComposition1.getSoilCompositionId()).orElseThrow();
+    assertEquals("KoKOKOkO", updatedSoilComposition.getCo());
+    assertEquals("1.1 mg/kg", updatedSoilComposition.getZn());
   }
 
 }
