048b58eb9ea4520407f7e3bc5331e60d15787092
FergeSS <sasha-uhnov@yandex.ru>
2024-11-15 19:15:11 +0300

    72 delete unnecessary import

--- src/main/java/agroscience/fields/v2/entities/FieldV2.java
+++ src/main/java/agroscience/fields/v2/entities/FieldV2.java
@@ -7,8 +7,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-
-import java.util.ArrayList;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
