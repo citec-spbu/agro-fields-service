ac71fdee89cc74c58b3e90627ec1597e95cf5013
FergeSS <sasha-uhnov@yandex.ru>
2024-11-15 19:19:40 +0300

    72 add the cond for delete a single contour

--- src/main/java/agroscience/fields/v2/entities/Contour.java
+++ src/main/java/agroscience/fields/v2/entities/Contour.java
@@ -55,6 +55,9 @@ public class Contour extends AbstractEntity {
   public void archive() {
     super.archive();
     soilCompositions.forEach(SoilComposition::archive);
+    if (field.getContours().size() == 1) {
+      field.setArchived(true);
+    }
   }
 
 }
