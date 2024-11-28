54b6a7b177845039fe42636366e60f1c89982c99
FergeSS <sasha-uhnov@yandex.ru>
2024-11-28 21:52:22 +0300

    100 add test for sort

--- src/test/java/agroscience/fields/CropRotationV2Test.java
+++ src/test/java/agroscience/fields/CropRotationV2Test.java
@@ -15,6 +15,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 import static agroscience.fields.SampleObjectGenerator.createSampleCropRotation;
@@ -80,6 +82,7 @@ public class CropRotationV2Test extends AbstractTest {
     CropRotationV2 cropRotation1 = createSampleCropRotation(field.getContours().get(0));
     cropRotationRepository.save(cropRotation1);
     CropRotationV2 cropRotation2 = createSampleCropRotation(field.getContours().get(0));
+    cropRotation2.setStartDate(LocalDate.of(2024, 10, 2));
     cropRotationRepository.save(cropRotation2);
     //When
     String url = "/api/v2/fields-service/contours/" + contourId + "/crop-rotations";
@@ -88,8 +91,8 @@ public class CropRotationV2Test extends AbstractTest {
     assertEquals(200, response.getStatusCode().value());
     assertNotNull(response.getBody());
     assertEquals(2, response.getBody().size());
-    assertEquals(response.getBody().get(0), cropRotationMapper.map(cropRotation1));
-    assertEquals(response.getBody().get(1), cropRotationMapper.map(cropRotation2));
+    assertEquals(cropRotationMapper.map(cropRotation1), response.getBody().get(1));
+    assertEquals(cropRotationMapper.map(cropRotation2), response.getBody().get(0));
   }
 
   @Test
